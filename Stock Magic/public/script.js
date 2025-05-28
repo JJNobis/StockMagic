ab5680111e2fb5d2b03c41d433dfbb07043702d6
Cody Newton <cyranium2022@gmail.com>
2025-05-28 13:27:20 -0700

    update page

--- Stock Magic/public/script.js
+++ Stock Magic/public/script.js
@@ -1,4 +1,4 @@
-const apiKey = 'W14I9M4P1PD5SM52';  //API key to connect to Stock Market
+const apiKey = '5ORFBD05O917SGOR';  //API key to connect to Stock Market
 //Spare API 
 //8G92EAMNYI9SW94C
 //W14I9M4P1PD5SM52
@@ -161,6 +161,8 @@ function getStockValue() {
 
             })
             localStorage.setItem("total", total);
+            document.getElementById("totalMoney").innerHTML = `$${(availableFunds + total).toFixed(2)}`;
+            document.getElementById("stockMoney").innerHTML = `$${total.toFixed(2)}`;
             // let previousTotal = parseFloat(localStorage.getItem("previousTotal"));
             // document.getElementById("priceChange").innerHTML = `Price change since last viewed: $${(total - previousTotal).toFixed(2)}`;
         })
@@ -229,7 +231,6 @@ function askForMoney() {
     } else {
         alert('Invalid input.')
     }
-    location.reload()
 }
 
 //calls app to update sql database with new available funds
@@ -249,7 +250,7 @@ function addMoneyToAccount(moneyIncome, accountID) {
         .catch(err => {
             console.log(err);
         });
-
+    location.reload();
 }
 //settings page start//
 
@@ -450,7 +451,8 @@ function buyStock() {
         document.getElementById("purchaseMessage").innerHTML = `Congratulations! You've purchased ${purchasedShares} shares of ${symbol.toUpperCase()}`;
         document.getElementById('result').innerHTML = "";
         localStorage.setItem("funds", availableFunds);
-        closeStock();
+
+
     } else {
         document.getElementById("purchaseMessage").innerHTML = `You only have enough funds to purchase up to ${buyAmount} shares.`;
     }
